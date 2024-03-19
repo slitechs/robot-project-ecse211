06a63b6660976a9a74f71645994e4194a2dd42f5
zzzmlssqnzzz <melissa.qian@mail.mcgill.ca>
2024-03-19 17:39:36 -0400

    modified code to not use touch sensor

--- project/catapult_test_position.py
+++ project/catapult_test_position.py
@@ -11,15 +11,23 @@ initial_position = catapult.get_position()
 
 while True:
     try:
+        for i in 10: 
+            #consistently shoots to the second target
+            #TODO: Try to shoot while mounted on the body of the robot 
+            catapult.set_position_relative(135)
+            time.sleep(1)
+            catapult.set_position_relative(initial_position)
+            time.sleep(5)
+        
         #motor.set_power(40)
-        if TOUCH_SENSOR.is_pressed():
-            print("Touch sensor is pressed")
+        #if TOUCH_SENSOR.is_pressed():
+            #print("Touch sensor is pressed")
             
             #Consistently gets into the second one
             #TODO: Increase length of the lever to increase distance 
-            catapult.set_position_relative(155)
-            time.sleep(0.5)
-            catapult.set_position(initial_position)
+            #catapult.set_position_relative(155)
+            #time.sleep(0.5)
+            #catapult.set_position(initial_position)
             
     except BaseException:  # capture all exceptions including KeyboardInterrupt (Ctrl-C)
         #motor.set_power(0)
