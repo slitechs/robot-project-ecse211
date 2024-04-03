9448615b8a43632418e62b81e4a3c602c376e27b
zzzmlssqnzzz <melissa.qian@mail.mcgill.ca>
2024-04-02 21:57:43 -0400

    Deleted unecessary comments

--- project/door_opener_test.py
+++ project/door_opener_test.py
@@ -15,34 +15,13 @@ i = 1 #Counter (1 to 10)
 while True:
     try:
         time.sleep(1)
-        door.set_position_relative(-45) #115 for 3rd,65 for 2nd, 50 for 1st
+        door.set_position_relative(-45) 
         print("Open: " + str(i))
         time.sleep(0.3) #0.35
         door.set_position(initial_position)
         print("Close: " + str(i))
         time.sleep(2)
         i +=1
-        
-        #door.set_power(-35)
-        #time.sleep(0.2)
-        #door.set_power(35)
-        #time.sleep(0.21)
-        #door.set_power(0)
-        #time.sleep(4)
-        
-        #motor.set_power(40)
-        #if TOUCH_SENSOR.is_pressed():
-            #print("Touch sensor is pressed")
-           
-    
-            
-            #Consistently gets into the second one
-            #TODO: Increase length of the lever to increase distance
-            #With increased length of lever, angle > 150 hits the table
-            #catapult.set_position_relative(158)
-        
-            #catapult.set_position_relative(150)
-            #catapult.set_position_relative(145)
        
             
     except BaseException:  # capture all exceptions including KeyboardInterrupt (Ctrl-C)
